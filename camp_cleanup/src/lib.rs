33b55f0d7385c883c0c7f1d31977c3fcaec73ce6
Georgi Petrov <petrov.d.george@gmail.com>
2023-04-23 10:36:47 +0300

    Cleanup parsing of input

--- camp_cleanup/src/lib.rs
+++ camp_cleanup/src/lib.rs
@@ -1,25 +1,30 @@
 use std::ops::RangeInclusive;
 
+#[allow(dead_code)]
 fn check_range_partial(bigger: RangeInclusive<usize>, smaller: RangeInclusive<usize>) -> bool {
     bigger.contains(smaller.start()) || bigger.contains(smaller.end())
 }
 
+#[allow(dead_code)]
 fn check_range_full(bigger: RangeInclusive<usize>, smaller: RangeInclusive<usize>) -> bool {
     bigger.contains(smaller.start()) && bigger.contains(smaller.end())
+}
+
+fn parse_line(line: &str) -> (usize, RangeInclusive<usize>, usize, RangeInclusive<usize>) {
+    let ranges = line.split(',').collect::<Vec<_>>();
+    let bounds1 = ranges[0].split('-').collect::<Vec<_>>();
+    let bounds2 = ranges[1].split('-').collect::<Vec<_>>();
+    let size1 = bounds1[1].parse::<usize>().unwrap() - bounds1[0].parse::<usize>().unwrap();
+    let size2 = bounds2[1].parse::<usize>().unwrap() - bounds2[0].parse::<usize>().unwrap();
+    let range1 = bounds1[0].parse::<usize>().unwrap()..=bounds1[1].parse::<usize>().unwrap();
+    let range2 = bounds2[0].parse::<usize>().unwrap()..=bounds2[1].parse::<usize>().unwrap();
+    (size1, range1, size2,range2)
 }
 
 pub fn camp_cleanup(input: &str, check_range: fn(RangeInclusive<usize>, RangeInclusive<usize>) -> bool) -> usize {
     let mut result = 0;
     for line in input.lines() {
-        let ranges = line.split(',').collect::<Vec<_>>();
-        let bounds1 = ranges[0].split('-').collect::<Vec<_>>();
-        let bounds2 = ranges[1].split('-').collect::<Vec<_>>();
-        
-        let size1 = bounds1[1].parse::<usize>().unwrap() - bounds1[0].parse::<usize>().unwrap();
-        let size2 = bounds2[1].parse::<usize>().unwrap() - bounds2[0].parse::<usize>().unwrap();
-        let range1 = bounds1[0].parse::<usize>().unwrap()..=bounds1[1].parse::<usize>().unwrap();
-        let range2 = bounds2[0].parse::<usize>().unwrap()..=bounds2[1].parse::<usize>().unwrap();
-
+        let (size1, range1, size2, range2) = parse_line(line);
         result += if size1 > size2 {
             check_range(range1, range2) as usize
         } else {
