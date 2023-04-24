86d536451e618fbf4cd42f6dd00669511c03e888
georgi.petrov@cognyte.com <georgi.petrov@cognyte.com>
2023-04-24 15:11:42 +0300

    use log instead of stdin

--- .github/scripts/create_issues.py
+++ .github/scripts/create_issues.py
@@ -1,49 +1,38 @@
 import os
 import sys
 import json
-import requests
+import re
+from github import Github
 
-# Get the warning messages from the input
-warnings = sys.stdin.read()
+# Read the log file specified as a command-line argument
+log_file = sys.argv[1]
+with open(log_file, "r") as f:
+    lines = f.readlines()
 
-print(f"Found warnings: {warnings} ")
+print("Creating issues for build warnings...")
+print(f"log file: {lines}")
 
-# Set up the GitHub API
-repo = os.getenv("GITHUB_REPOSITORY")
-token = os.getenv("GITHUB_TOKEN")
-headers = {
-    "Accept": "application/vnd.github+json",
-    "Authorization": f"token {token}",
-}
+# Filter the lines to get only warnings
+warnings = [line.strip() for line in lines if line.startswith("warning:")]
 
-# Get all open issues
-issues_url = f"https://api.github.com/repos/{repo}/issues?state=open"
-response = requests.get(issues_url, headers=headers)
-open_issues = json.loads(response.text)
+# Initialize GitHub API client
+gh = Github(os.environ["GITHUB_TOKEN"])
+repo = gh.get_repo(os.environ["GITHUB_REPOSITORY"])
 
-print(f"Found {len(open_issues)} open issues")
-
-# Iterate over the warnings and create issues
-for warning in warnings.strip().split("\n"):
-    # Format the title of the issue
-    print(f"Creating issue for warning: {warning}")
-    issue_title = warning.split('\n', 1)[0]
-    title = f"Build warning: { issue_title }"
+# Create issues for each warning
+for warning in warnings:
+    # Extract the warning message without the newline character
+    title = warning.split("\n", 1)[0].replace("\\", "")
 
     # Check if an issue with the same title already exists
-    existing_issue = None
-    for issue in open_issues:
-        if issue["title"] == title:
-            existing_issue = issue
-            break
+    existing_issues = repo.get_issues(state="open", labels=["build-warning"])
+    issue_exists = any(issue.title == title for issue in existing_issues)
 
-    if existing_issue is None:
-        # Create the issue
-        issue_data = {
-            "title": title,
-            "body": f"```\n{warning}\n```",
-            "labels": ["build-warning"],
-        }
-        create_url = f"https://api.github.com/repos/{repo}/issues"
-        res = requests.post(create_url, headers=headers, json=issue_data)
-        print(f"Created issue: {res.status_code} {res.text}")
+    if not issue_exists:
+        # Create a new issue
+        issue = repo.create_issue(
+            title=title,
+            body=f"**Warning message:**\n```\n{warning}\n```",
+            labels=["build-warning"],
+        )
+        print(f"Created issue: {issue.number} {issue.html_url}")
