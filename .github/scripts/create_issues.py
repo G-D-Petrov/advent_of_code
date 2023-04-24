4a25908dce50a3a6a00780edeeb669f0411f9404
georgi.petrov@cognyte.com <georgi.petrov@cognyte.com>
2023-04-24 15:31:36 +0300

    Fix warning parsing

--- .github/scripts/create_issues.py
+++ .github/scripts/create_issues.py
@@ -4,6 +4,11 @@ import json
 import re
 from github import Github
 
+# Strip escape sequences for terminal colors
+def strip_ansi_escape(text):
+    ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]')
+    return ansi_escape.sub('', text)
+
 # Read the log file specified as a command-line argument
 log_file = sys.argv[1]
 with open(log_file, "r") as f:
@@ -12,8 +17,8 @@ with open(log_file, "r") as f:
 print("Creating issues for build warnings...")
 print(f"log file: {lines}")
 
-# Filter the lines to get only warnings
-warnings = [line.strip() for line in lines if line.startswith("warning:")]
+# Filter the lines to get only warnings and strip escape sequences
+warnings = [strip_ansi_escape(line.strip()) for line in lines if "warning" in line]
 
 # Initialize GitHub API client
 gh = Github(os.environ["GITHUB_TOKEN"])
@@ -22,7 +27,8 @@ repo = gh.get_repo(os.environ["GITHUB_REPOSITORY"])
 # Create issues for each warning
 for warning in warnings:
     # Extract the warning message without the newline character
-    title = warning.split("\n", 1)[0].replace("\\", "")
+    print(f"warning: {warning}")
+    title = warning.split(":")[1]
 
     # Check if an issue with the same title already exists
     existing_issues = repo.get_issues(state="open", labels=["build-warning"])
